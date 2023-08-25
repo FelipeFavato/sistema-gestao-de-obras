3a4a1d057df29926a134be7be19e88ae4cbff7a5
FelipeFavato <felipelopesfavato@gmail.com>
2023-08-25 16:29:29 -0300

    Alterando arquivo pom.xml e vite.config.js para integracao

--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -4,3 +4,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+// function cadastrar() {
+//   fetch('http://localhost:8080/cadastrar',
+//     {
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       method: 'Post',
+//       body: JSON.stringify({ a: 1, b: 2 }) => Formato dos dados
+//     })
+//     .then(function (res) { console.log(res) })
+//     .catch(function (res) { console.log(res) })
+// }
