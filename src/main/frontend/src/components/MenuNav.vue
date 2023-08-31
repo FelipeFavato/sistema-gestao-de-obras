58441d0ea4fa05369404235e51afc936b33dab16
FelipeFavato <felipelopesfavato@gmail.com>
2023-08-31 17:30:22 -0300

    Mexendo um pouco no CSS

---
+++ src/main/frontend/src/components/MenuNav.vue
@@ -0,0 +1,91 @@
+<script setup>
+
+import { RouterView, useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const redirect = (path) => {
+  router.push(path)
+}
+
+</script>
+
+<template>
+
+  <main>
+    <div class="botao-inicio">
+      <button
+        type="button"
+        class="btn btn-dark"
+        @click="redirect('/')"
+      >Início</button>
+    </div>
+
+    <div class="menu-options">
+      <button
+        type="button"
+        class="btn btn-ligth"
+        @click="redirect('/obra')"
+      >Obra</button>
+
+      <button
+        type="button"
+        class="btn btn-ligth"
+        @click="redirect('/obracusto')"
+      >ObraCusto</button>
+
+      <button
+        type="button"
+        class="btn btn-ligth"
+        @click="redirect('/localuso')"
+      >LocalUso</button>
+
+      <button
+        type="button"
+        class="btn btn-ligth"
+        @click="redirect('/fornecedor')"
+      >Fornecedores</button>
+
+      <button
+        type="button"
+        class="btn btn-ligth"
+        @click="redirect('/produtoservico')"
+      >ProdutoServico</button>
+    </div>
+
+    <div class="botao-perfil">
+      <button
+        type="button"
+        class="btn btn-dark"
+        @click="redirect('/perfil')"
+      >👤</button>
+    </div>
+
+  </main>
+
+  <RouterView />
+
+</template>
+
+<style scoped>
+main {
+  margin: 25px 5px 40px 5px;
+  display: flex;
+  align-content: space-around;
+  justify-content: space-between;
+}
+
+/* .botao-inicio {
+} */
+
+.menu-options {
+  display: flex;
+  width: 900px;
+  justify-content: space-between;
+}
+
+/* .botao-perfil {
+} */
+
+
+</style>
