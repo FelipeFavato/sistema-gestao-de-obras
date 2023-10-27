ba9f6935b472a5e04649ed81bfc538445fb23d11
FelipeFavato <felipelopesfavato@gmail.com>
2023-10-27 13:40:10 -0300

    Começando a dar uma cara legal para a tela de login

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,12 +1,34 @@
-<script setup>
+<script>
 
-  import { RouterView } from 'vue-router';
-  import MenuNav from './components/MenuNav.vue';
+import { RouterView } from 'vue-router';
+import MenuNav from './components/MenuNav.vue';
+
+export default {
+  data () {
+    return {
+      loginPage: true,
+    }
+  },
+
+  components: {
+    MenuNav,
+  },
+
+  computed: {
+    isNotLoginPage() {
+      return this.$route.path !== "/login"
+    }
+  },
+
+  mounted () {
+    console.log(this.$route) 
+  }
+}
 
 </script>
 
 <template>
-  <header class="App-header">
+  <header v-if="isNotLoginPage" class="App-header">
     <MenuNav />
   </header>
 
--- src/main/frontend/src/views/Login.vue
+++ src/main/frontend/src/views/Login.vue
@@ -59,34 +59,107 @@ export default {
 </script>
 
 <template>
-  <h2>login</h2>
+  
+  <!-- Main Container -->
+  <div class="container d-flex justify-content-center align-items-center min-vh-100">
+    <!-- Login Container -->
+    <div class="row border rounded-5 p-3 bg-white shadow box-area">
+      <!-- Left Box -->
+      <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #252925;">
+        <div class="featured-image mb-3 mt-3">
+          <img src="../assets/imagens/capacete.png" alt="capacete" class="img-fluid" style="width: 250px;">
+        </div>
+        <p class="text-white fs-2" style="font-family:'Courier New', Courier, monospace; font-weight: 700;">HUB Construções</p>
+        <!-- <small class="text-white text-warp text-center" style="width: 17rem; font-family: 'Courier New', Courier, monospace;"></small> -->
+      </div>
+      <!-- Right Box -->
+      <div class="col-md-6 rounded-4 d-flex flex-column right-box">
+        <div class="ajuste-tamanho">
+            <form action="POST" class="ajuste-tamanho">
 
-  <form action="POST">
+              <div>
+                <!-- Email -->
+                <div class="mb-3">
+                  <label for="email-input" class="form-label bold">Email:</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="email-input"
+                      placeholder="usuario@email.com"
+                      v-model="email"
+                      >
+                </div>
 
-    <input
-      type="text"
-      placeholder="usuario@gmail.com"
-      v-model="email"
-    >
+                <!-- Senha -->
+                <div class="mb-3">
+                  <label for="senha-input" class="form-label bold">Senha:</label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      id="senha-input"
+                      placeholder="******"
+                      v-model="senha"
+                      >
+                </div>
+              </div>
+              <!-- Lembrar de mim? -->
 
-    <input
-      type="text"
-      placeholder="Sua senha"
-      v-model="senha"
-    >
-    
-    <button
-      id="login-button"
-      type="button"
-      @click="login"
-    >Login
-    </button>
+              <!-- Resposta requisição com falha. -->
+              <div>
+                <p
+                  class="red-letter"
+                  v-if="httpStatusCode === 403"
+                  >Usuário e/ou senha inválidos.
+                </p>
+              </div>
 
-  </form>
+              <!-- Entrar -->
+              <div class="mb-3">
+                <button
+                  type="button"
+                  class="btn btn-success  light-green w-100 fs-6"
+                  @click="login"
+                >Login</button>
+              </div>
 
-  <p v-if="httpStatusCode === 403">Usuário e/ou senha inválidos.</p>
+            </form>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
 
 </template>
 
-<style>
+<style setup>
+
+body{
+  background-color: rgb(171, 180, 178);
+}
+
+.red-letter {
+  color: red;
+}
+
+.company-color {
+  background-color: #252925;
+}
+
+.box-area{
+  width: 930px;
+}
+
+.border {
+  border-radius: 1px solid #000;
+}
+
+.ajuste-tamanho {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
 </style>
