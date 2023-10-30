9fd99a28b8231c660b4a630473d99e0ff6cbf495
FelipeFavato <felipelopesfavato@gmail.com>
2023-10-30 14:18:10 -0300

    preparando para o merge com a main

--- src/main/java/com/gobra/sistemagestaodeobras/model/Usuario.java
+++ src/main/java/com/gobra/sistemagestaodeobras/model/Usuario.java
@@ -36,8 +36,8 @@ import lombok.Setter;
 public class Usuario implements UserDetails {
   
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_generator_usuario")
-  @SequenceGenerator(name = "seq_generator_usuario", sequenceName = "SEQUENCIA_USUARIO", initialValue = 1, allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_gen_usuario")
+  @SequenceGenerator(name = "seq_gen_usuario", sequenceName = "SEQUENCIA_USUARIOS", initialValue = 1, allocationSize = 1)
   private Integer codigo;
 
   @Column(name = "nome", length = 100, unique = true)
