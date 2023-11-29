ab964c08468131f5c84b026878fae0020809f0ff
FelipeFavato <felipelopesfavato@gmail.com>
2023-11-29 10:58:04 -0300

    Commitando a branch telegram para iniciar o desenvolvimento do bot

--- src/main/frontend/src/views/Configuracoes.vue
+++ src/main/frontend/src/views/Configuracoes.vue
@@ -1,183 +1,14 @@
 <script>
-
-import axios from 'axios';
-import Plotly from 'plotly.js-dist';
-import { useRouter } from 'vue-router';
-
 export default {
   data () {
     return {
-      useRouter: useRouter(),
-      httpStatus: '',
-      localStorageToken: null,
-      comprasInfo: [],
-      custoLocalUsoInfo: [],
-      processedCompraData: []
     }
   },
     
   methods: {
-    // Método para redirecionar para a página de login.
-    redirectToLogin () {
-      this.useRouter.push('/login');
-    },
-    // Método para recuperar o token do localStorage e preencher 'this.localStorageToken'.
-    getLocalStorageToken () {
-      this.localStorageToken = localStorage.getItem('token');
-    },
-    // Método para setar o 'this.httpStatusCode' com os casos de sucesso e erro.
-    setHttpStatusCode (succesError) {
-      this.httpStatus = succesError;
-    },
-    // Método para validar o StatusHttp da requisição. Casos de token expirado.
-    validateHttpStatus (status) {
-      this.setHttpStatusCode(status);
-      this.httpStatus === 403 ? this.redirectToLogin(): null;
-    },
-    // Método para validar o login baseado no token.
-    validateLogin () {
-      !this.localStorageToken ? this.redirectToLogin() : null;
-    },
-    // Método para buscar os dados das compras
-    fetchComprasInfoDB (callback) {
-      axios.get('/api/compra',
-      {
-        headers: {
-          Authorization: this.localStorageToken
-        }
-      }).then(res => {
-        this.comprasInfo = res.data;
-        if (callback) callback();
-      }).catch(error => {
-
-      });
-    },
-    extractMonth (date) {
-      return Number(date.split('-')[1]);
-    },
-    processComprasData () {
-      // const processedData = [];
-      const meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-      let somaMes = 0;
-      for (let mes of meses) {
-        for (let compra of this.comprasInfo) {
-          if (this.extractMonth(compra.dataVencimento) === mes) {
-            somaMes += compra.valorFinal;
-          }
-        }
-        this.processedCompraData.push(somaMes);
-      }
-    },
-    genGraph () {
-      const meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
-      // const gastoCompras = this.processComprasData();
-
-      const compras = {
-        x: meses,
-        // y: gasto acumulado por mês, de acordo com a data de vencimento?
-        y: this.processedCompraData,
-        type: 'bar',
-        marker: {
-          color: '#292525'
-        },
-        name: 'Compras'
-      };
-
-      const maoDeObra = {
-        x: meses,
-        y: [0],
-        type: 'bar',
-        marker: {
-          color: 'darkorange'
-        },
-        name: 'Mão de Obra'
-      };
-
-      const data = [maoDeObra, compras];
-
-      const layout = {
-        title: 'Gastos acumulados',
-        font: { size: 15 },
-        barmode: 'stack',
-        showLegend: true,
-        // legend: {
-        //   x: 1,
-        //   xanchor: 'left',
-        //   y: 1
-        // },
-        // xaxis: { type: 'date' },
-      };
-
-      const config = {
-        responsive: true,
-        displayModeBar: false,
-        // staticPlot: true
-      };
-
-      Plotly.newPlot("tester", data, layout, config);
-    },
-    fetchCustoLocalUsoInfoDB (callback) {
-      axios.get('/api/dashboard/valorlocaluso',
-      {
-        headers: {
-          Authorization: this.localStorageToken
-        }
-      }).then(res => {
-        this.custoLocalUsoInfo = res.data;
-        if (callback) callback();
-      }).catch(error => {
-
-      })
-    },
-    fixCurrency (dinheiroDouble) {
-      if (dinheiroDouble === null) {
-        return null
-      } else {
-        const valorFormatado = dinheiroDouble.toLocaleString('pt-BR', {
-          style: 'currency',
-          currency: 'BRL'
-        });
-
-        return valorFormatado;
-      }
-    },
-    genCustoLocalUsoGraph () {
-      const locais = [];
-      const valores = [];
-      const par = [];
-      const text = [];
-
-      for (let obj of this.custoLocalUsoInfo) {
-        locais.push(obj['nomeLocalUsoObra']);
-        valores.push(obj['valorTotal']);
-        par.push('')
-        text.push(this.fixCurrency(obj['valorTotal']))
-      }
-
-      const data = [{
-        type: "treemap",
-        labels: locais,
-        values: valores,
-        parents: par,
-        text: text,
-        textinfo: "label+text+percent parent",
-        hoverinfo: "label+text"
-      }];
-
-      Plotly.newPlot('custoLocalUso', data)
-    }
   },
 
   mounted () {
-    this.getLocalStorageToken();
-    this.validateLogin();
-    // this.fetchComprasInfoDB(() => {
-    //   this.processComprasData();
-    //   this.genGraph();
-    // });
-    // this.fetchCustoLocalUsoInfoDB(() => {
-    //   this.genCustoLocalUsoGraph();
-    // });
   }
 }
 
@@ -185,16 +16,6 @@ export default {
 
 <template>
   <h2>Configurações </h2>
-
-  <!-- <div>
-    <p v-for="(compra, i) in comprasInfo" :key="i">
-      {{ compra.dataVencimento }} - {{ compra.dataPagamento }} - {{ compra.valorFinal }}
-    </p>
-  </div> -->
-
-  <!-- <div id="tester" style="width:800px;height:400px;"></div> -->
-
-  <!-- <div id="custoLocalUso" style="width:800px;height:400px;"></div> -->
 
 </template>
 
