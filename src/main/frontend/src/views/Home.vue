a03512a8f5c3212d9cdd484ef20b645931783375
FelipeFavato <felipelopesfavato@gmail.com>
2024-03-18 16:07:30 -0300

    Pagina de previsao finalizada

--- src/main/frontend/src/views/Home.vue
+++ src/main/frontend/src/views/Home.vue
@@ -95,6 +95,17 @@ export default {
     <div class="card margin-10" style="width: 18rem;">
       <div class="card-body column">
         <div>
+          <h5 class="card-title">Atualização 18.02.24</h5>
+          <p class="card-text">Página de Previsao de custos</p>
+          <p>1. Página finalizada</p>
+          <p></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="card margin-10" style="width: 18rem;">
+      <div class="card-body column">
+        <div>
           <h5 class="card-title">Atualização 08.02.24</h5>
           <p class="card-text">Página de Compras</p>
           <p>1. Campos obrigatórios ajustados.</p>
